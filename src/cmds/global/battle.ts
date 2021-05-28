4c9525fd5bc1de5a646a00c95fe12cefaf9c1967
adrifcastr <adrifcastr@gmail.com>
2021-05-28 20:38:32 +0200

    basic concepts

--- src/cmds/global/battle.ts
+++ src/cmds/global/battle.ts
@@ -1,12 +1,18 @@
-import { MessageEmbed } from 'discord.js';
-import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js';
+import { CommandInteraction, MessageComponentInteraction, MessageActionRow, MessageButton, Message, MessageEmbed } from 'discord.js';
 import { Command } from 'src/@types/Util';
-
 export async function run(interaction: CommandInteraction): Promise<void> {
 
     const startrow = new MessageActionRow()
-    .addComponent(new MessageButton().setStyle('PRIMARY').setLabel('Start battle'));
-    return interaction.editReply('Battle format: `Gen 8 Random Singles Battle (OU)`', { components: [startrow] });       
+    .addComponent(new MessageButton().setStyle('PRIMARY').setLabel('Start battle').setCustomID('startbattle'));
+    interaction.editReply('Battle format: `Gen 8 Random Singles Battle (OU)`', { components: [startrow] }); 
+	
+	const message = await interaction.fetchReply() as Message;
+
+	const filter = (i: MessageComponentInteraction) => i.customID === 'startbattle' && i.user.id === interaction.user.id;
+
+	message.awaitMessageComponentInteractions(filter, { time: 15000, maxComponents: 1 })
+	.then(x => x.first()?.update('Battle started.', { components: []}))
+	.catch(console.error);
 }
 
 export const info: Command['info'] = {
