bb6538106d4c71f84d48bf374216409694cdb6f9
adrifcastr <adrifcastr@gmail.com>
2021-06-26 20:37:00 +0200

    fix

--- src/@types/Util.d.ts
+++ src/@types/Util.d.ts
@@ -1,5 +1,4 @@
-/* eslint-disable no-redeclare */
-/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-redeclare */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Discord from 'discord.js';
 import Interactions from 'handlers/Interactions';
@@ -11,41 +10,51 @@ export function log(message: string, files: string[]): boolean;
 export function LoadCommands(): Promise<void>;
 export function DeployCommands(): Promise<void>;
 export function fetchJSON(url: string): Promise<unknown>;
-export function truncate(str: string, length: number, useWordBoundary: boolean): string;
+export function truncate(
+  str: string,
+  length: number,
+  useWordBoundary: boolean
+): string;
 export function normalize(num: number): string;
 
 declare module 'discord.js' {
-    interface Client {
-        commands: Discord.Collection<string, Command>;
-        events: Discord.Collection<string, Event>;
-        owner: string;
-    }
+  interface Client {
+    commands: Discord.Collection<string, Command>;
+    events: Discord.Collection<string, Event>;
+    owner: string;
+  }
 }
 
 declare global {
-    declare namespace NodeJS {
-        export interface Process {
-            showdown: Discord.Client;
-        }
+  declare namespace NodeJS {
+    export interface Process {
+      showdown: Discord.Client;
     }
+  }
 }
 
 interface InteractionsInterface {
-    SlashCommands(interaction: Discord.CommandInteraction, Util: unknown): Promise<void>;
+  SlashCommands(
+    interaction: Discord.CommandInteraction,
+    Util: unknown
+  ): Promise<void>;
 }
 
-interface secondsToDifferenceSettings {
-    enableSeconds: boolean
+interface SecondstodifferenceSettings {
+  enableSeconds: boolean;
 }
 
 interface Command {
-    data: Discord.ApplicationCommandData;
-    async run(interaction: Discord.CommandInteraction, options: Discord.Collection<String,Discord.CommandInteractionOption>): Promise<void>;
+  data: Discord.ApplicationCommandData;
+  run(
+    interaction: Discord.CommandInteraction,
+    options: Discord.Collection<String, Discord.CommandInteractionOption>
+  ): Promise<void>;
 }
 
 interface Event {
-    name: string;
-    once?: boolean;
-    process?: boolean
-    async run(...args: unknown[]): Promise<void>;
+  name: string;
+  once?: boolean;
+  process?: boolean;
+  run(...args: unknown[]): Promise<void>;
 }
