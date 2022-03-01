3f3e862b1713c3dbbb5fc13f4be6868601c2bf7d
Adrian Fernandez Castro <adrian.fernandez.castro@rehau.com>
2022-03-01 18:16:21 +0100

    a lot of initial stuffs

--- src/showdown.ts
+++ src/showdown.ts
@@ -1,14 +1,15 @@
 import { config } from 'dotenv';
-config();
 
 import '@sapphire/plugin-logger/register';
 import { LogLevel, SapphireClient } from '@sapphire/framework';
+
+config();
 
 const client = new SapphireClient({
 	intents: ['GUILDS'],
 	logger: {
 		level: LogLevel.Debug
-	},
+	}
 });
 
 await client.login();
