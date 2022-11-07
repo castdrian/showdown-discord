f21187d9dc8aaa0396393256b78cf147eb5626d2
Adrian Fernandez Castro <adrian.fernandez.castro@rehau.com>
2022-11-07 10:37:46 +0100

    add info command

---
+++ src/@types/last-commit-log.d.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+/* eslint-disable no-unused-vars */
+declare module 'last-commit-log' {
+	export default class LCL {
+		constructor(path: string);
+		getLastCommit(): Promise<Info>;
+	}
+
+	interface Info {
+		gitTag: string;
+		gitBranch: string;
+		gitRemote: string;
+		gitUrl: string;
+		shortHash: string;
+		hash: string;
+		subject: string;
+		sanitizedSubject: string;
+		body: string;
+		committer: {
+			date: string;
+			relativeDate: string;
+			name: string;
+			email: string;
+		};
+		author: {
+			date: string;
+			relativeDate: string;
+			name: string;
+			email: string;
+		};
+	}
+}
---
+++ src/commands/info.ts
@@ -0,0 +1,106 @@
+import { type ApplicationCommandRegistry, Command, RegisterBehavior, version as sversion } from '@sapphire/framework';
+import { CommandInteraction, Message, version } from 'discord.js';
+import { sendErrorToUser } from '#util/functions';
+import { default as lcl } from 'last-commit-log';
+import { default as ts } from 'typescript';
+import { cpus, totalmem } from 'os';
+
+export class Info extends Command {
+	public override async chatInputRun(interaction: CommandInteraction) {
+		await interaction.deferReply();
+		const message = await interaction.fetchReply();
+		// try catch the entire thing and send an error message if it fails
+		try {
+			const guilds = this.container.client.guilds.cache.size;
+			const users = this.container.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
+			const channels = this.container.client.channels.cache.size;
+			const { uptime } = this.container.client;
+			const uptimeString = new Date(uptime!).toISOString().substr(11, 8);
+			const os = process.platform;
+			const cpu = process.cpuUsage();
+			const cpuUsage = Math.round((cpu.user + cpu.system) / 1000000);
+			const cpuCount = cpus().length;
+			const cpuModel = cpus()[0].model;
+			const osString = `${os} ${cpuCount}x ${cpuModel}`;
+			const cpuUsageString = `Using ${cpuUsage}% of ${cpuCount} CPU cores`;
+			const memory = process.memoryUsage().heapUsed / 1024 / 1024;
+			const memoryString = `${memory.toFixed(2)} MB / ${Math.round(totalmem() / 1024 / 1024)} MB`;
+			const shard = this.container.client.shard?.ids[0] ?? 0;
+			const shardCount = this.container.client.shard?.count ?? 1;
+			const shardString = `${shard + 1}/${shardCount}`;
+			const lclInstance = new lcl(process.cwd());
+			const lastCommit = await lclInstance.getLastCommit();
+			const { gitUrl, shortHash, sanitizedSubject, author } = lastCommit;
+			const commitDate = new Date(parseInt(author.date, 10) * 1000);
+			const commitDateString = commitDate.toDateString();
+			const commitString = `[${shortHash}](${gitUrl}) - ${sanitizedSubject} \`${commitDateString}\``;
+			const node = `[${process.version}](https://nodejs.org/en/download/releases/)`;
+			const tsver = `[v${ts.version}](https://www.typescriptlang.org/download)`;
+			const latency = this.container.client.ws.ping;
+			const djs = `[v${version}](https://discord.js.org)`;
+			const sapphire = `[v${sversion}](https://sapphirejs.dev)`;
+
+			const embed = {
+				title: 'Showdown! Info',
+				thumbnail: {
+					url: this.container.client.user?.displayAvatarURL() ?? ''
+				},
+				description: `**Guilds:** ${guilds}\n**Users:** ${users}\n**Channels:** ${channels}\n**Uptime:** ${uptimeString}\n**Latency:** ${latency} ms\n**System:** ${osString}\n**CPU Usage:** ${cpuUsageString}\n**Memory Usage:** ${memoryString}\n**Shard:** ${shardString}\n**Current Commit:** ${commitString}\n**Node:** ${node}\n**TypeScript:** ${tsver}\n**Discord.js:** ${djs}\n**Sapphire:** ${sapphire}`,
+				color: 0x5865f2
+			};
+
+			const components = [
+				{
+					type: 1,
+					components: [
+						{
+							type: 2,
+							label: 'Contact',
+							style: 5,
+							url: 'discord://-/users/224617799434108928'
+						},
+						{
+							type: 2,
+							label: 'Support Server',
+							style: 5,
+							url: 'https://discord.gg/JRwVqEXHxp'
+						},
+						{
+							type: 2,
+							label: 'GitHub',
+							style: 5,
+							url: 'https://github.com/castdrian/showdown'
+						},
+						{
+							type: 2,
+							label: 'Add to Server',
+							style: 5,
+							url: 'https://discord.com/api/oauth2/authorize?client_id=847595833347801118&permissions=314368&scope=applications.commands%20bot'
+						}
+					]
+				}
+			];
+
+			await interaction.editReply({ embeds: [embed], components });
+
+			// get git, node, and typescript data
+		} catch (error: any) {
+			if (message instanceof Message) {
+				await sendErrorToUser(error, message, interaction);
+			}
+		}
+	}
+
+	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+		registry.registerChatInputCommand(
+			{
+				name: this.name,
+				description: 'Show Showdown! info'
+			},
+			{
+				guildIds: process.env.DEV_GUILD_ID ? [process.env.DEV_GUILD_ID] : undefined,
+				behaviorWhenNotIdentical: RegisterBehavior.Overwrite
+			}
+		);
+	}
+}
